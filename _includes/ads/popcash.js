65d1daa1ed8a20c518351e714a8ec14437d056d7
Akhmad Syahroni <akhmadsyahroni.my.id@gmail.com>
2025-08-22 03:01:58 +0700

    add popcash js to article

---
+++ _includes/ads/popcash.js
@@ -0,0 +1,1 @@
+<script>var uid="487104",wid="739485",pop_tag=document.createElement("script");pop_tag.src="//cdn.popcash.net/show.js",document.body.appendChild(pop_tag),pop_tag.onerror=function(){(pop_tag=document.createElement("script")).src="//cdn2.popcash.net/show.js",document.body.appendChild(pop_tag)};</script>
