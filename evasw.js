b595e7251fa257fe3d09aa648562b0ff9ff48abc
Akhmad Syahroni <akhmadsyahroni.my.id@gmail.com>
2025-08-22 02:52:19 +0700

    add evasw.js

---
+++ evasw.js
@@ -0,0 +1,4 @@
+---
+permalink: /evasw.js
+---
+importScripts('https://feldot.com/v1/sw-import.js?h=waWQiOjExOTc4NzksInNpZCI6MTQ2MDQwOSwid2lkIjo3MTA5NzksInNyYyI6MiwicG0iOjAsIm10IjowfQ==eyJ&d=asibuka.com');
