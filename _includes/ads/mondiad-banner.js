e94a9c53acb3a154732bf9c34b0123a2b7786d49
Akhmad Syahroni <akhmadsyahroni.my.id@gmail.com>
2025-08-31 20:04:29 +0700

    center mondiad banner ad

--- _includes/ads/mondiad-banner.js
+++ _includes/ads/mondiad-banner.js
@@ -1,4 +1,6 @@
 {% unless page.is_amp %}
+<div class='ads text-center'>
 <div class='mondiad-300x250-banner-ad' data-mndbanid="ecf71b32-c495-4156-ba3f-a73888b5af25"></div>
 <script>Defer.dom(".mondiad-300x250-banner-ad",1000,"loaded",function(){var a=document.createElement("script");a.src="https://ss.mrmnd.com/banner.js",document.body.appendChild(a),console.info("Mondiad 300x250 Banner ad is loaded.")},{rootMargin:"150%"});</script>
+</div>
 {% endunless %}
